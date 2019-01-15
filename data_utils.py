b4df0467fe40e9840b344d80a87b3a56c47d0541
LongguangWang <wanglongguang15@nudt.edu.cn>
2019-01-15 11:05:23 +0800

    V4

--- data_utils.py
+++ data_utils.py
@@ -3,6 +3,7 @@ from PIL import Image
 import os
 import torch
 from torch.utils.data.dataset import Dataset
+import math
 
 
 class TestsetLoader(Dataset):
@@ -16,15 +17,21 @@ class TestsetLoader(Dataset):
         LR0 = Image.open(dir + '/' + 'lr_' + str(idx+1).rjust(2, '0') + '.png')
         LR1 = Image.open(dir + '/' + 'lr_' + str(idx+2).rjust(2, '0') + '.png')
         LR2 = Image.open(dir + '/' + 'lr_' + str(idx+3).rjust(2, '0') + '.png')
+        W, H = LR1.size
+        # H and W should be divisible by 2
+        W = int(W // 2) * 2
+        H = int(H // 2) * 2
+        LR0 = LR0.crop([0, 0, W, H])
+        LR1 = LR1.crop([0, 0, W, H])
+        LR2 = LR2.crop([0, 0, W, H])
 
-        H, W = LR1.size
-        LR1_bicubic = LR1.resize((H*self.upscale_factor, W*self.upscale_factor), Image.BICUBIC)
+        LR1_bicubic = LR1.resize((W*self.upscale_factor, H*self.upscale_factor), Image.BICUBIC)
         LR1_bicubic = np.array(LR1_bicubic, dtype=np.float32) / 255.0
 
         LR0 = np.array(LR0, dtype=np.float32) / 255.0
         LR1 = np.array(LR1, dtype=np.float32) / 255.0
         LR2 = np.array(LR2, dtype=np.float32) / 255.0
-
+        # extract Y channel for LR inputs
         LR0_y, _, _ = rgb2ycbcr(LR0)
         LR1_y, _, _ = rgb2ycbcr(LR1)
         LR2_y, _, _ = rgb2ycbcr(LR2)
@@ -35,7 +42,7 @@ class TestsetLoader(Dataset):
         LR = np.concatenate((LR0_y, LR1_y, LR2_y), axis=2)
 
         LR = toTensor(LR)
-
+        # generate Cr, Cb channels using bicubic interpolation
         _, SR_cb, SR_cr = rgb2ycbcr(LR1_bicubic)
         return LR, SR_cb, SR_cr
     def __len__(self):
